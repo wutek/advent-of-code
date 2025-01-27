7b38ce1a32ad3c6e889e46d021e8ba8fc2906abc
Damian Woźniak <dwoxniak@gmail.com>
2025-01-27 22:46:35 +0100

    Update 2015 day 1 solution

--- 2015/1.js
+++ 2015/1.js
@@ -1,13 +1,18 @@
-const input = `()())`
+// Advent of Code 2015 day 1 solution
+// https://adventofcode.com/2015/day/1
+
+const input = '()())'
+
+console.log('Part 1:', aoc_2015_1_a(input))
+console.log('Part 2:', aoc_2015_1_b(input))
+
 
 function aoc_2015_1_a(input) {
     return input.split('').reduce((t, c) => t + (c === '(' ? 1 : -1), 0)
 }
 
 function aoc_2015_1_b(input) {
-    let floor = 0
-
-    for (let i = 0; i <= input.length; i++) {
+    for (let i = 0, floor = 0; i < input.length; i++) {
         floor += (input[i] === '(' ? 1 : -1)
 
         if (floor < 0) {
@@ -15,6 +20,3 @@ function aoc_2015_1_b(input) {
         }
     }
 }
-
-console.log(aoc_2015_1_a(input))
-console.log(aoc_2015_1_b(input))
